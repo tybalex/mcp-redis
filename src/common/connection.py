d9bfb715a38a3c6bb63b6b25fd2193559a05d0b5
notgurev <notgurev@gmail.com>
2025-05-19 12:17:57 +0300

    Add REDIS_DB env variable for db selection

--- src/common/connection.py
+++ src/common/connection.py
@@ -16,23 +16,43 @@ class RedisConnectionManager:
     def get_connection(cls, decode_responses=True) -> Redis:
         if cls._instance is None:
             try:
-                redis_class: Type[Union[Redis, RedisCluster]] = redis.cluster.RedisCluster if REDIS_CFG["cluster_mode"] else redis.Redis
+                if REDIS_CFG["cluster_mode"]:
+                    redis_class: Type[Union[Redis, RedisCluster]] = redis.cluster.RedisCluster
+                    connection_params = {
+                        "host": REDIS_CFG["host"],
+                        "port": REDIS_CFG["port"],
+                        "username": REDIS_CFG["username"],
+                        "password": REDIS_CFG["password"],
+                        "ssl": REDIS_CFG["ssl"],
+                        "ssl_ca_path": REDIS_CFG["ssl_ca_path"],
+                        "ssl_keyfile": REDIS_CFG["ssl_keyfile"],
+                        "ssl_certfile": REDIS_CFG["ssl_certfile"],
+                        "ssl_cert_reqs": REDIS_CFG["ssl_cert_reqs"],
+                        "ssl_ca_certs": REDIS_CFG["ssl_ca_certs"],
+                        "decode_responses": decode_responses,
+                        "lib_name": f"redis-py(mcp-server_v{__version__})",
+                        "max_connections_per_node": 10 
+                    }
+                else:
+                    redis_class: Type[Union[Redis, RedisCluster]] = redis.Redis
+                    connection_params = {
+                        "host": REDIS_CFG["host"],
+                        "port": REDIS_CFG["port"],
+                        "db": REDIS_CFG["db"],
+                        "username": REDIS_CFG["username"],
+                        "password": REDIS_CFG["password"],
+                        "ssl": REDIS_CFG["ssl"],
+                        "ssl_ca_path": REDIS_CFG["ssl_ca_path"],
+                        "ssl_keyfile": REDIS_CFG["ssl_keyfile"],
+                        "ssl_certfile": REDIS_CFG["ssl_certfile"],
+                        "ssl_cert_reqs": REDIS_CFG["ssl_cert_reqs"],
+                        "ssl_ca_certs": REDIS_CFG["ssl_ca_certs"],
+                        "decode_responses": decode_responses,
+                        "lib_name": f"redis-py(mcp-server_v{__version__})",
+                        "max_connections": 10
+                    }
                 
-                cls._instance = redis_class(
-                    host=REDIS_CFG["host"],
-                    port=REDIS_CFG["port"],
-                    username=REDIS_CFG["username"],
-                    password=REDIS_CFG["password"],
-                    ssl=REDIS_CFG["ssl"],
-                    ssl_ca_path=REDIS_CFG["ssl_ca_path"],
-                    ssl_keyfile=REDIS_CFG["ssl_keyfile"],
-                    ssl_certfile=REDIS_CFG["ssl_certfile"],
-                    ssl_cert_reqs=REDIS_CFG["ssl_cert_reqs"],
-                    ssl_ca_certs=REDIS_CFG["ssl_ca_certs"],
-                    decode_responses=decode_responses,
-                    lib_name=f"redis-py(mcp-server_v{__version__})",
-                    **({"max_connections_per_node": 10} if REDIS_CFG["cluster_mode"] else {"max_connections": 10})
-                )
+                cls._instance = redis_class(**connection_params)
 
             except redis.exceptions.ConnectionError:
                 print("Failed to connect to Redis server", file=sys.stderr)
